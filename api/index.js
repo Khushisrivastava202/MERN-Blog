0105abe01260ea9b95bdfede84c9c6e32d7a1f72
Pranaysrv <82716033+Khushisrivastava202@users.noreply.github.com>
2024-01-22 00:07:14 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import User from "./models/user.model.js";
+import bcryptjs from "bcryptjs"
+
   
 dotenv.config();
+
+
 
 mongoose.connect(process.env.MONGO)
 .then( () =>{
@@ -11,9 +16,36 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 
-const app=express()
+const app=express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json({message:'API is working'});
+});
+ 
+app.post("/signup", async(req,res)=>{
+    const {username,email,password}=req.body;
+
+    if (!username || !email || !password || !username.trim() || !email.trim() || !password.trim()) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser = new User({
+        username,
+        email,
+        password:hashedPassword,
+    });
+    try {
+        await newUser.save();
+        res.json({ message: "SignUp Successful" });
+    } catch (error) {
+        console.error("Error during signup:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
 
 app.listen(3000,  () => {
     console.log("Server is running on port 3000");
 
-})
+});
