929cf3c9c223f86f29aaf23229a5f77e14945b09
Pranaysrv <82716033+Khushisrivastava202@users.noreply.github.com>
2024-01-24 03:49:42 +0530

    add Google OAth functionality with firebase

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link,useNavigate} from "react-router-dom";
 import { useDispatch ,useSelector} from "react-redux";
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice"
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData,setFormData]=useState({});
@@ -75,6 +76,9 @@ const handleSubmit = async (e) => {
         <span className='pl-3' >Loading.....</span>
         </>
         ) : "Sign In"}</Button>
+
+
+        <OAuth/>
 
       </form>
       <div className=" flex gap-2 text-sm mt-5">
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@
 import {Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link,useNavigate} from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 
 export default function SignUp() {
@@ -83,6 +84,7 @@ const handleSubmit = async (e) => {
         <span className='pl-3' >Loading.....</span>
         </>
         ) : "Sign Up"}</Button>
+        <OAuth/>
 
       </form>
       <div className=" flex gap-2 text-sm mt-5">
